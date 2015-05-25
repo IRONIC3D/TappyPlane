44295ee25568354eba98b315b10f73d676b4efa7
Iyad Horani <i3d@ironic3d.com>
2015-05-25 13:41:48 +1000

    Initial Commit

---
+++ Tappy PlaneTests/Tappy_PlaneTests.m
@@ -0,0 +1,40 @@
+//
+//  Tappy_PlaneTests.m
+//  Tappy PlaneTests
+//
+//  Created by Iyad Horani on 25/05/2015.
+//  Copyright (c) 2015 IRONIC3D. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Tappy_PlaneTests : XCTestCase
+
+@end
+
+@implementation Tappy_PlaneTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
